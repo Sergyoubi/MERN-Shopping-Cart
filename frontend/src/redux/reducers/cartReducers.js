4bf2f64751c9da0105385efeb1e3c576648f98fd
Sergy <sergyoubi@gmail.com>
2021-09-09 11:48:37 +0300

    Update cartReducers.js

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -3,20 +3,18 @@ import * as actionTypes from '../constants/cartConstants';
 export const cartReducer = ( state = {cartItems: []}, action ) => {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
-      const item = action.payload; //in action.payload we get the item we would like to add
+      const item = action.payload; 
       const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
         return {
-          ...state, //spread the and go to the cartItem array:
+          ...state,
           cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x) 
-          //update cartItems: map through the old array and check the current product if it's equal to the existing item
-          //if it's True, we set it to the new item(action.payload), else we set it to be the current elemt we mapping through 
         }
       } else { 
         return {
           ...state,
-          cartItems: [...state.cartItems, item] //push new item in the array
+          cartItems: [...state.cartItems, item]
         }
       }
     case actionTypes.REMOVE_FROM_CART:
